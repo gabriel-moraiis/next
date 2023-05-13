9387544ab119086dea29aa4f748c268676301275
Gabriel Morais <gabriel.moraiis95@gmail.com>
2023-05-13 11:10:10 -0300

    add initital commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,63 @@
+import { GetServerSideProps } from "next"
+import Image from "next/image";
+import Stripe from "stripe";
+import { stripe } from "yarn/lib/stripe";
+import { ImageContainer, ProductContainer, ProductDetails } from "yarn/styles/pages/product"
+
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    description: string | null;
+    imageUrl: string;
+    price: Stripe.Price;
+}
+}
+
+export default function Product({ product }: ProductProps) {
+
+  return(
+    <ProductContainer>
+      <ImageContainer>
+        <Image
+        src={product.imageUrl}
+        width={520}
+        height={480}
+        alt=""
+        />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<any, { id: string }> = async ({ params }) => {
+  const productId = params!.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        description: product.description,
+        price,
+      }
+    }
+  }
+}
+
+
