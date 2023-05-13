876b3d1863ef8036b6ce026abcf249e037c2205c
Gabriel Morais <gabriel.moraiis95@gmail.com>
2023-05-13 11:15:50 -0300

    add site title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
 
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }}/>
+        <title>Ignite Shop</title>
       </Head>
       <body>
         <Main />
